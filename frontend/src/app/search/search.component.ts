ef68fcef3c68c2eebc15accc3b8a7ff2f0317c44
Chau Le <chaule19@vt.edu>
2022-04-15 13:06:26 -0400

    add image link

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -11,19 +11,19 @@ export class SearchComponent implements OnInit {
   tutorial_cards: any[] = [{
     title: "Cloud Tutorial",
     description: "The purpose of the tutorial is to serve as support, as well as a challenge, for individual students' knowledge acquisition, but it can also be used for training collaboration abilities.",
-    image: ""
+    image: "https://www.wwt.com/api/attachments/5e6026402dab3d0093840a23/thumbnail?width=600"
   }, {
     title: "Java Tutorial",
     description: "The purpose of the tutorial is to serve as support, as well as a challenge, for individual students' knowledge acquisition, but it can also be used for training collaboration abilities.",
-    image: ""
+    image: "https://dev.java/assets/images/java-logo-vert-blk.png"
   }, {
     title: "Python Tutorial",
     description: "The purpose of the tutorial is to serve as support, as well as a challenge, for individual students' knowledge acquisition, but it can also be used for training collaboration abilities.",
-    image: ""
+    image: "https://avatars.githubusercontent.com/u/1525981?s=280&v=4"
   }, {
     title: "NodeJS Tutorial",
     description: "The purpose of the tutorial is to serve as support, as well as a challenge, for individual students' knowledge acquisition, but it can also be used for training collaboration abilities.",
-    image: ""
+    image: "https://nodejs.org/static/images/logo-hexagon-card.png"
   }
 ];
 listArr: any[] = []
