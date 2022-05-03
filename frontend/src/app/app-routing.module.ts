72bd640fe9df60ee10b595d6bf0ac6426774a39f
Linh <linhpham@vt.edu>
2022-05-02 20:31:40 -0400

    About

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { AppComponent } from './app.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 import { HomeComponent } from './home/home.component';
 import { RecordingComponent } from './recording/recording.component';
+import { InfoComponent } from './info/info.component';
 
-const routes: Routes = [{ path: 'recording/:file', component: RecordingComponent }, { path: 'favorites', component: FavoritesComponent }, 
+const routes: Routes = [{ path: 'recording/:file', component: RecordingComponent }, { path: 'about', component: InfoComponent }, { path: 'favorites', component: FavoritesComponent }, 
 { path: '', component: HomeComponent },
  { path: '*', component: AppComponent },];
 
