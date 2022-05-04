acd682848df6df84c1815a8527e4f4ce41c3a69d
Linh <linhpham@vt.edu>
2022-05-04 00:03:25 -0400

    ready production

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,14 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend/dist/frontend')))
 app.use('/', indexRouter);
-app.use('/recordings', recordingsRouter);
 
+app.use('/recordings', recordingsRouter);
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/frontend', "index.html"))
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
